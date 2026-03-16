edd16cf98b18f40965f169465c7ac6de89a28c52
ajakeydev <142936243+ajakeydev@users.noreply.github.com>
2026-03-16 09:48:39 -0500

    added header and date() instance on page load
    added header and date() instance on page load

--- proPortfolio/src/app/app.ts
+++ proPortfolio/src/app/app.ts
@@ -5,8 +5,25 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('Aaron\'s A.K.A. ajakeydev\'s portfolio is under construction. Thank you for visiting! 🥰');
+  protected readonly title = signal(
+    "Aaron's A.K.A. ajakeydev's portfolio is under construction. Thank you for visiting! 🥰",
+  );
+
+  public digits = " ";
+  public cycle = " ";
+  public forDate = " ";
+  public date = new Date();
+
+  constructor(
+
+  ) {
+    // this.logger(this.date)
+  }
+
+  logger(x: any): void {
+    console.log(x)
+  }
 }
