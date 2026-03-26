3f32c0f34bbbc6d0a2b592d71f4827dbc6423a5a
ajakeydev <142936243+ajakeydev@users.noreply.github.com>
2026-03-26 09:53:27 -0500

    added event listeners on Home link
    added event listeners on Home link. Added hr line rule and styling to it

--- proPortfolio/src/app/initComponents/header/header.ts
+++ proPortfolio/src/app/initComponents/header/header.ts
@@ -1,4 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, Directive, HostListener, Renderer2, ElementRef } from '@angular/core';
+
+// @Directive({
+//   selector: '[appHover]',
+//   standalone: true
+// })
 
 @Component({
   selector: 'app-header',
@@ -6,6 +11,7 @@ import { Component, signal } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
+
 export class Header {
 
   protected readonly headerText = signal('Test header text')
@@ -15,8 +21,24 @@ export class Header {
   public forDate = " ";
   public date = new Date();
 
-  constructor(
+  public theta: number = 0.01
+  public mat2 = [
+    Math.cos(this.theta), Math.sin(this.theta),
+    -Math.sin(this.theta), Math.cos(this.theta)
+  ]
 
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
   ) { }
+
+  // TODO: need to manipulate font-size and stuff without causing header height to change with it... ============================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  @HostListener('mousenter') onEnter(): void {
+    this.renderer.setStyle(this.el.nativeElement, 'font-size', '2rem');
+  }
+
+  @HostListener('mouseleave') onLeave(): void {
+    
+  }
 
 }
