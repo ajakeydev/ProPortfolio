16a4ed13e9624d1d15f4b0fee65e6fa96f98ca87
ajakeydev <142936243+ajakeydev@users.noreply.github.com>
2026-05-08 08:14:18 -0500

    added comments/notes to tsconfig.app.json and main.ts
    added comments/notes to tsconfig.app.json and main.ts

--- proPortfolio/src/main.ts
+++ proPortfolio/src/main.ts
@@ -4,3 +4,4 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+// TODO: the title of the index.html in the app-root component changes to 404 not found when it should, however, it does not change back to Portfolio -- Aaron Mitchell. Should research and fix NOOP
