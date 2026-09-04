b17e2697a532b85e2c698fc9082f5d029b3dc207
ajakeydev <142936243+ajakeydev@users.noreply.github.com>
2026-09-04 07:32:57 -0500

    added gltfScene to hellogltf.ts
    added gltfScene to hellogltf.ts

--- proPortfolio/src/app/pages/landing/isodeca-imported/hellogltf/hellogltf.ts
+++ proPortfolio/src/app/pages/landing/isodeca-imported/hellogltf/hellogltf.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, computed, signal, effect } from '@angular/core';
-import { NgtArgs, beforeRender } from 'angular-three';
+import { NgtArgs, beforeRender, extend } from 'angular-three';
 import { gltfResource } from 'angular-three-soba/loaders';
 import { NgtsEnvironment } from 'angular-three-soba/staging';
 import * as THREE from 'three';
+
+extend(THREE);
 
 const uniforms = {
   uTime: { value: 0.0 },
@@ -23,6 +25,7 @@ export class Hellogltf {
     uTime: { value: 0.0 },
     uAngle: { value: 0.0 },
   }
+  gltfScene = signal<THREE.Group | null>(null);
   constructor (
 
   ) { }
